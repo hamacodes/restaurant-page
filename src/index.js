de6c0990168bab88c6536c7eb017c6a298157542
Hama <pashazad@msu.edu>
2024-08-22 18:42:22 -0400

    feat: Add createAboutPage and event listeners for about button in index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import createHomePage from "./home.js";
 import createMenuPage from "./menu.js";
+import createAboutPage from "./about.js";
+
+// button event listeners
+const homeButton = document.querySelector('#home');
+homeButton.addEventListener('click', createHomePage);
+
+const menuButton = document.querySelector('#menu');
+menuButton.addEventListener('click', createMenuPage);
+
+const aboutButton = document.querySelector('#about');
+aboutButton.addEventListener('click', createAboutPage);
